34fe947a8e56d0cf7b01316d7320581fc7696d21
Dennis Lindquist <dennislindquist@gmail.com>
2021-03-15 20:16:00 +0100

    Add project files.

---
+++ BookWebShop/Models/SoldBook.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookWebShop.Models
+{
+    class SoldBook
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Author { get; set; }
+
+        public int Price { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public int PurchasedDate { get; set; }
+
+        public int UserId { get; set; }
+    }
+}
---
+++ BookWebShop/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookWebShop.Models
+{
+    class User
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Password { get; set; } = "Codic2021";
+
+        public DateTime LastLogin { get; set; }
+
+        public DateTime SessonTimer { get; set; }
+
+        public bool IsActive { get; set; } = true;
+
+        public bool IsAdmin { get; set; } = false;
+    }
+}
