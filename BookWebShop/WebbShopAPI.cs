ff476986a26625ee0e9d10262d76aa29e074324e
Dennis Lindquist <dennislindquist@gmail.com>
2021-03-18 23:07:14 +0100

    iul

---
+++ BookWebShop/WebbShopAPI.cs
@@ -0,0 +1,61 @@
+﻿using BookWebShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.Text;
+
+namespace BookWebShop
+{
+    class WebbShopAPI
+    {
+        public int Login(string username, string password)
+        {
+            return 0;
+        }
+
+        //public DateTime Logout(int userId)
+        //{
+
+        //}
+
+        public void GetCategories()
+        {
+
+        }
+
+        public string GetCategories(string keyword)
+        {
+            return keyword;
+        }
+
+        public int GetBook(int bookId)
+        {
+            return bookId;
+        }
+
+        public string GetBooks(string keyword)
+        {
+            return keyword;
+        }
+
+        public string GetAuthors(string keyword)
+        {
+            return keyword;
+        }
+
+        public bool BuyBook(int userId, int bookId)
+        {
+            return false;
+        }
+
+        public int Ping(int userId)
+        {
+            return 0;
+        }
+
+        public bool Register(string name, string password, bool passwordVerify)
+        {
+            return false;
+        }
+    }
+}
