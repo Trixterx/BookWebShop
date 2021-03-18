bd23582fa9bc2f7385afab8060569467ecb5f1a3
Dennis Lindquist <dennislindquist@gmail.com>
2021-03-18 21:43:18 +0100

    added seeder

--- BookWebShop/Program.cs
+++ BookWebShop/Program.cs
@@ -9,38 +9,9 @@ namespace BookWebShop
     {
         static void Main(string[] args)
         {
-            CreateMockData();
+            Seeder.Seed();
         }
-        public static void CreateMockData()
-        {
-            using (var db = new WebbShopContext())
-            {
-                if (db.Users.Count() == 0)
-                {
-                    db.Users.Add(new User { Name = "Administrator", Password = "CodicRulez", IsActive = true, IsAdmin = true });
-                    db.Users.Add(new User { Name = "TestCustomer", Password = "Codic2021", IsActive = true, IsAdmin = false });
-                    db.SaveChanges();
-                }
 
-                if (db.BookCategories.Count() == 0)
-                {
-                    db.BookCategories.Add(new BookCategory { Name = "Horror" });
-                    db.BookCategories.Add(new BookCategory { Name = "Humor" });
-                    db.BookCategories.Add(new BookCategory { Name = "Science Fiction" });
-                    db.BookCategories.Add(new BookCategory { Name = "Romance" });
-                    db.SaveChanges();
-                }
-
-                if (db.Books.Count() == 0)
-                {
-                    db.Books.Add(new Book { Title = "Cabal (Nightbreed)", Author = "Clive Barker", Price = 250, Amount = 3, Category = db.BookCategories.FirstOrDefault(c => c.Name == "Horror") });
-                    db.Books.Add(new Book { Title = "The Shining", Author = "Stephen King", Price = 200, Amount = 2, Category = db.BookCategories.FirstOrDefault(c => c.Name == "Horror") });
-                    db.Books.Add(new Book { Title = "Doctor Sleep", Author = "Stephen King", Price = 200, Amount = 1, Category = db.BookCategories.FirstOrDefault(c => c.Name == "Horror") });
-                    db.Books.Add(new Book { Title = "I Robot", Author = "Isaac Asimov", Price = 150, Amount = 4, Category = db.BookCategories.FirstOrDefault(c => c.Name == "Science Fiction") });
-                    db.SaveChanges();
-                }
-            }
-        }
         public User LogIn(string username, string password, bool isAdmin)
         {
 
