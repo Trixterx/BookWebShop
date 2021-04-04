584d0f1613d423e0754d52c0e7e1fcb08df229c0
Dennis Lindquist <dennislindquist@gmail.com>
2021-04-05 01:25:19 +0200

    jlk

--- BookWebShopFrontend/BookWebShopFrontend/View/Home/CustomerHomeMenu.cs
+++ BookWebShopFrontend/BookWebShopFrontend/View/Home/CustomerHomeMenu.cs
@@ -14,13 +14,7 @@ namespace BookWebShopFrontend.View.Home
             Console.WriteLine("| Customer                                               |");
             Console.WriteLine("----------------------------------------------------------");
             Console.WriteLine("1. Book Menu");
-            Console.WriteLine("2. List Categories with Search");
-            Console.WriteLine("3. List of Books in Category");
-            Console.WriteLine("4. List of Books");
-            Console.WriteLine("5. Info about book");
-            Console.WriteLine("6. List Books with search");
-            Console.WriteLine("7. List Books with Author Search");
-            Console.WriteLine("8. Buy Book");
+            Console.WriteLine("2. Category Menu");
             Console.WriteLine("0. Logout");
         }
     }
